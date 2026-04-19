43b572060b42311b24f0e1a18658061170ce18fa
SaiemAziz <sayemazizchy@gmail.com>
2026-04-19 20:22:36 +0600

    build issue fixed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <Box as='section' bg={'#061321'} minH={'100vh'}>
-      <App />
+      <Box as="section" bg={"#061321"} minH={"100vh"}>
+        <App />
       </Box>
     </ChakraProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
